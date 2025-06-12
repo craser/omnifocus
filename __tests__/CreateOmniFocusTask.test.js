5b0fe628081894b2174229752422d95369234baf
craser <chris@raser.io>
2025-06-11 22:27:55 -0700

    ROLLUP: removed test - no exported class from script any more

--- __tests__/CreateOmniFocusTask.test.js
+++
@@ -1,8 +0,0 @@
-const CreateOmniFocusTask = require('src/jxa/create-omnifocus-task');
-
-jest.mock('child_process');
-
-test('CreateOmniFocusTask should exist', () => {
-    expect(CreateOmniFocusTask).toBeTruthy();
-});
-
